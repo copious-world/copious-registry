97e9cccfa84902c2e28e731e7d8f80cd62b33492
Richard Albert Leddy <richardaleddy@gmail.com>
2023-02-11 15:53:13 -0800

    Reduce code by deferring to extra-file-class

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const DirWatcherHandler = require('./lib/directory_watch_handler.js')
 const ObjFileDirLoader = require('./lib/object_file_loader.js');
-let CachingProcess = require('./lib/caching_process')
 //
 const {Registry,FileLists} = require('./lib/registry.js')
 //
 module.exports.DirWatcherHandler = DirWatcherHandler
-module.exports.CachingProcess = CachingProcess
 module.exports.ObjFileDirLoader = ObjFileDirLoader
 module.exports.Registry = Registry
 module.exports.FileLists = FileLists
