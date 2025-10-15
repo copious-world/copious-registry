85a5bba34300c27234dedbce026317dde524e345
Richard Leddy <richardaleddy@gmail.com>
2025-10-15 13:24:23 -0700

    added comments which may be found in the local description

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ module.exports.ObjFileDirLoader = ObjFileDirLoader
 module.exports.Registry = Registry
 module.exports.FileLists = FileLists
 //
+/**
+ * This module provides a basic search infrastructure that can be configured.
+ * 
+ * The module gets data by observing events dealing with the publication of the 
+ * to directories. The basic behavior for endpoint operations and pub/sub can be configured by extending 
+ * the watcher classes.
+ * 
+ * It builds indexes which are sorted according to configurable matching algorithms.
+ * 
+ * It provides data backup and reloading. (Serialization and deserialization)
+ * And, manages file lists.
+ * 
+ * 
+ */
